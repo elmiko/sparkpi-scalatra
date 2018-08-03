26587418a8222061bfcb0aa17992717fa82ac272
Michael Reynolds <reynoldsm88@gmail.com>
2018-08-03 08:44:01 -0400

    my documentation was wrong. moving scala source files into src/main/scala. also fixing the empty context root mapping behavior.

--- src/main/scala/io/radanalytics/examples/scalatra/sparkpi/SparkPiServlet.scala
+++ src/main/scala/io/radanalytics/examples/scalatra/sparkpi/SparkPiServlet.scala
@@ -4,6 +4,11 @@ import org.apache.spark.{SparkConf, SparkContext}
 import org.scalatra.{Ok, ScalatraServlet}
 
 class SparkPiServlet extends ScalatraServlet {
+
+    get("/"){
+        Ok( "Scala Scalatra SparkPi server running. Add the 'sparkpi' route to this URL to invoke the app." )
+    }
+
     get( "/sparkpi" ) {
         val spark = new SparkContext( new SparkConf().setAppName( "Radanalytics IO Scalatra Tutorial" ) )
         val sparkPi = new SparkPI( spark,2 ).calculate()
